82ee4939566c937a2325fba99780909bbee9e995
Bohdan Serdinov <bogdan.serdinov.2002@gmail.com>
2024-11-10 01:32:48 +0100

    finished

--- client_test.go
+++ client_test.go
@@ -1,14 +1,22 @@
-package sdk_versioning
+package sdk_versioning_test
 
 import (
 	"context"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"sdk_versioning"
 )
 
 func TestCoingeckoClient(t *testing.T) {
-	client := NewClient()
+	client := sdk_versioning.NewClient()
 
 	ctx := context.Background()
 
-	client.(ctx, "bitcoin")
+	price, err := client.GetPrice(ctx, "bitcoin")
+	require.NoError(t, err, "unable to get price")
+
+	assert.Greater(t, price.Bitcoin.Usd, 0, "price should be greater than 0")
 }
